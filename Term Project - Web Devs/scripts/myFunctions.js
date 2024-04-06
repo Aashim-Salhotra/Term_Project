3ae6932ee097c258e17505c70fcb00c99aa3ec22
nishantnepal3 <chocowafers4@gmail.com>
2024-04-06 10:43:14 -0400

    Created script folder and added js file and some videos

---
+++ Term Project - Web Devs/scripts/myFunctions.js
@@ -0,0 +1,14 @@
+
+
+//Thank you alert
+function tyAlert() {
+	alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
+
+//Burpees video window
+function burpees() {
+	window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+	document.getElementById('video').play();
+}
+
+
