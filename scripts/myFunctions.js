294600a52c64f935f50a4d2b4ecfe5046825d81b
nishantnepal3 <157234155+nishantnepal3@users.noreply.github.com>
2024-04-10 12:44:21 -0400

    Add files via upload
    made changes to script

---
+++ scripts/myFunctions.js
@@ -0,0 +1,13 @@
+
+
+//Thank you alert
+function tyAlert() {
+	alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
+
+//Mountain climber video window
+function mtnClimber() {
+	window.open("scripts/mtn.html", "_blank", "width=610, height=360");
+	document.getElementById('video').play();
+}
+
