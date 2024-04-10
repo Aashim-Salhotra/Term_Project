5a864b564f9b8c888f272bf2b0b4f472f842639a
nishantnepal3 <chocowafers4@gmail.com>
2024-04-10 12:26:04 -0400

    Removed the media file blurpees and plank from the js file

--- Term Project - Web Devs/scripts/myFunctions.js
+++ Term Project - Web Devs/scripts/myFunctions.js
@@ -5,18 +5,6 @@ function tyAlert() {
 	alert("Thank you for your interest in our fitness club. We will contact you soon.");
 }
 
-//Burpees video window
-function burpees() {
-	window.open("scripts/burpees.html", "_blank", "width=610, height=360");
-	document.getElementById('video').play();
-}
-
-//Plank video window
-function plank() {
-	window.open("scripts/plank.html", "_blank", "width=610, height=360");
-	document.getElementById('video').play();
-}
-
 //Mountain climber video window
 function mtnClimber() {
 	window.open("scripts/mtn.html", "_blank", "width=610, height=360");
